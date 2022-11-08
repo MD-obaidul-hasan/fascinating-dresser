04cd1d8a80ae1ad99f27a019df1ae0eeef8280b7
Obaidul hasan <md.obaidulhasnlimu@gmaiol.comgit config --global user.name Obaidul>
2022-11-08 15:41:13 +0600

    service design> git commit -m'services

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <p className='text-2xl font-bold text-orange-600'> Services</p>
+                <h2 className='text-5xl font-semibold'> My Service Area</h2>
+                <p className='my-5'>Event decoration servisis is avalable  in capital city in our country.<br/>
+                Outsite capital need booking first and need extra qurier service charge.<br/>
+                so need contract with me.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
